d11f19743390a8321317922a4449aa313cfeb053
wynter <u0lee.dev@gmail.com>
2025-11-20 17:12:59 +0900

    Feat: github page publish 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
